8f70199255cfbd2a804b4caa80e9dd153deb5efd
Harsh Shandilya <me@msfjarvis.dev>
2024-07-14 01:05:29 +0530

    chore: refresh Nix setup

--- flake.nix
+++ flake.nix
@@ -1,30 +1,53 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "devshell for a NodeJS project";
 
-    systems.url = "github:msfjarvis/flake-systems";
+  inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
 
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
+  inputs.systems.url = "github:msfjarvis/flake-systems";
 
-    flake-compat = {
-      url = "github:nix-community/flake-compat";
-      flake = false;
-    };
-  };
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.devshell.inputs.flake-utils.follows = "flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default =
-        pkgs.mkShell {nativeBuildInputs = with pkgs; [nodejs yarn];};
-    });
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.inputs.systems.follows = "systems";
+
+  inputs.flake-compat.url = "github:nix-community/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs =
+    {
+      nixpkgs,
+      devshell,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          bash = {
+            interactive = "";
+          };
+
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+
+          packages = with pkgs; [
+            nodejs
+            yarn
+          ];
+        };
+      }
+    );
 }
